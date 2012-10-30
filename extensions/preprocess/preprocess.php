24bd5b5029a258bf1ada74b982a8cfd49221f17b
topsitemakers <support@topsitemakers.com>
2012-10-30 20:34:02 +0100

    Added opening code

---
+++ extensions/preprocess/preprocess.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Main preprocess function.
+ *
+ * Created by: Topsitemakers
+ * http://www.topsitemakers.com/
+ */
+function ultima_preprocess(&$vars) {
+  
+  global $user;
+
+  // 
+
+}
