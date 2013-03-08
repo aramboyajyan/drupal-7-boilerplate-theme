89488ab342536adb996ed977bf09bc49d81e508c
Topsitemakers <support@topsitemakers.com>
2013-03-08 22:06:19 +0100

    Typo fix

--- theme-settings.php
+++ theme-settings.php
@@ -18,7 +18,7 @@ function ultima_form_system_theme_settings_alter(&$form, $form_state) {
   $form['ultima_tracking_head'] = array(
     '#type' => 'textarea',
     '#title' => t('Head tracking scripts'),
-    '#description' => t('Paste here tracking code (e.g. Google Analytics, Gauge etc.) that requires to be inserted within the body tag.'),
+    '#description' => t('Paste here tracking code (e.g. Google Analytics, Gauge etc.) that requires to be inserted within the head tag.'),
     '#default_value' => theme_get_setting('ultima_tracking_head'),
   );
 
