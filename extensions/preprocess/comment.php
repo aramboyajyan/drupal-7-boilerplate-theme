ffd39b78416f0b189fa64b5a1a2a347bccc78979
Topsitemakers <support@topsitemakers.com>
2013-08-09 11:57:38 +0200

    adding separate comment templates for node types

--- extensions/preprocess/comment.php
+++ extensions/preprocess/comment.php
@@ -12,4 +12,8 @@ function ultima_preprocess_comment(&$vars) {
   // Add odd/even classes to comments.
   $vars['classes_array'][] = $vars['zebra'];
 
+  // Separate comment templates per node content type. This will automatically
+  // look for "comment--[machine-node-type].tpl.php" files.
+  $vars['theme_hook_suggestions'][] = 'comment__' . $vars['node']->type;
+
 }
