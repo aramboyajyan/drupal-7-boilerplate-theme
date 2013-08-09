260463827b54b4fde983757471beaa8e727bf61a
Topsitemakers <support@topsitemakers.com>
2013-08-09 11:59:04 +0200

    altering the permalink URL

--- extensions/preprocess/comment.php
+++ extensions/preprocess/comment.php
@@ -16,4 +16,8 @@ function ultima_preprocess_comment(&$vars) {
   // look for "comment--[machine-node-type].tpl.php" files.
   $vars['theme_hook_suggestions'][] = 'comment__' . $vars['node']->type;
 
+  // Override the default "permalink" and change the href so it does not open
+  // the separate "/comment/[cid]" page.
+  $vars['permalink'] = l('Permalink' . $vars['comment']->cid, 'node/' . $vars['node']->nid, array('fragment' => 'comment-' . $vars['comment']->cid));
+
 }
