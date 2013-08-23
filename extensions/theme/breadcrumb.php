bd3ca25d0bcc3e50eecdfa0ccce3713b956111ce
Topsitemakers <support@topsitemakers.com>
2013-08-23 19:00:38 +0200

    adding breadcrumb settings and grouping settings on theme settings page in fieldsets

--- extensions/theme/breadcrumb.php
+++ extensions/theme/breadcrumb.php
@@ -15,7 +15,8 @@ function ultima_breadcrumb(&$vars) {
 
   // Commonly used things.
   global $user;
-  $divider = '<span class="divider">|</span>';
+  $divider_text = theme_get_setting('ultima_breadcrumb_divider');
+  $divider = '<span class="divider">' . $divider_text . '</span>';
   // If we are on node page, get the node.
   if (arg(0) == 'node' && arg(1) && !arg(2)) {
     $node = node_load(arg(1));
@@ -34,9 +35,18 @@ function ultima_breadcrumb(&$vars) {
 
     $title = '';
     if ($include_title) {
-      // Include current page's title.
+      // Include current page's title. Trim down the length to specific number
+      // of characters so long page titles don't break the layout.
       $title  = $divider . '</li>';
-      $title .= '<li class="active">' . drupal_get_title() . '</li>';
+      $raw_title = drupal_get_title();
+      $title_limit = theme_get_setting('ultima_breadcrumb_title_limit');
+      if (strlen($raw_title) > $title_limit) {
+        $trimmed_title = substr(drupal_get_title(), 0, $title_limit) . '...';
+      }
+      else {
+        $trimmed_title = $raw_title;
+      }
+      $title .= '<li class="active">' . $trimmed_title . '</li>';
     }
 
     // Provide a navigational heading to give context for breadcrumb links to
--- theme-settings.php
+++ theme-settings.php
@@ -13,22 +13,52 @@
  */
 function ultima_form_system_theme_settings_alter(&$form, $form_state) {
 
-  // Tracking scripts for analytics. These will be inserted before the ending
-  // head tag (html.tpl.php).
-  $form['ultima_tracking_head'] = array(
+  /**
+   * Tracking scripts.
+   */
+  $form['tracking'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Tracking Scripts'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+  );
+  // Header scripts inserted before the ending head tag in html.tpl.php.
+  $form['tracking']['ultima_tracking_head'] = array(
     '#type' => 'textarea',
     '#title' => t('Head tracking scripts'),
     '#description' => t('Paste here tracking code (e.g. Google Analytics, Gauge etc.) that requires to be inserted within the head tag.'),
     '#default_value' => theme_get_setting('ultima_tracking_head'),
   );
-
-  // Tracking scripts for analytics. These will be inserted before the ending
-  // body tag (html.tpl.php).
-  $form['ultima_tracking_body'] = array(
+  // Header scripts inserted before the ending body tag in html.tpl.php.
+  $form['tracking']['ultima_tracking_body'] = array(
     '#type' => 'textarea',
     '#title' => t('Body tracking scripts'),
     '#description' => t('Paste here tracking code (e.g. Piwik etc.) that requires to be inserted within the body tag.'),
     '#default_value' => theme_get_setting('ultima_tracking_body'),
+  );
+
+  /**
+   * Breadcrumb settings.
+   */
+  $form['breadcrumb'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Breadcrumb Settings'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE, 
+  );
+  // Breadcrumb divider text.
+  $form['breadcrumb']['ultima_breadcrumb_divider'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Divider'),
+    '#description' => t('Enter the textual divider that should be inserted between the breadcrumb links.'),
+    '#default_value' => theme_get_setting('ultima_breadcrumb_divider'),
+  );
+  // Page title maximum length in breadcrumbs.
+  $form['breadcrumb']['ultima_breadcrumb_title_limit'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Title limit'),
+    '#description' => t('Enter the number of characters that should be displayed in breadcrumb page title.'),
+    '#default_value' => theme_get_setting('ultima_breadcrumb_title_limit'),
   );
 
   // Copyright line.
