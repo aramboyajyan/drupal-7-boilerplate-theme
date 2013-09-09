2f92058cf3ed80acd71025bf6d272f9ace74ae17
Topsitemakers <support@topsitemakers.com>
2013-09-09 09:52:38 +0200

    separate comment wrapper templates per node content type

--- extensions/preprocess/comment-wrapper.php
+++ extensions/preprocess/comment-wrapper.php
@@ -9,6 +9,8 @@
  */
 function ultima_preprocess_comment_wrapper(&$vars) {
   
-  // 
+  // Separate comment templates per node content type. This will automatically
+  // look for "comment--[machine-node-type].tpl.php" files.
+  $vars['theme_hook_suggestions'][] = 'comment_wrapper__' . $vars['node']->type;
 
 }
