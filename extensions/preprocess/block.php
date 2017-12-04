77a504e6a3354b1a8201bf02d4b048d34ba6d520
Aram Boyajyan <aramboyajyan@users.noreply.github.com>
2017-12-04 13:04:50 +0100

    Adding clearfix to all block wrappers

--- extensions/preprocess/block.php
+++ extensions/preprocess/block.php
@@ -8,8 +8,8 @@
  * http://www.topsitemakers.com/
  */
 function ultima_preprocess_block(&$vars) {
+  $vars['classes_array'][] = 'clearfix';
   
   // Add class names that match region name.
   $vars['classes_array'][] = 'block-region-' . $vars['elements']['#block']->region;
-
 }
